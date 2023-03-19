f3d205b0f668ebf8d0fee28136f2a5a11c3c60ab
nandu2212 <nanda22121998@gmail.com>
2023-03-19 12:13:46 +0530

    updation done

--- Routes/update.js
+++ Routes/update.js
@@ -5,18 +5,16 @@ const BookModal=require('../model/BOOKS')
 const router=express.Router()
 
 router.put('/:id',async(req,res)=>{
-    await BookModal.findOne({_id:req.params.id}).then((book)=>{
-        
-        if(book){
-            const updatedBook=req.body
-             BookModal.updateOne({Name:req.body.Name,Author:req.body.Author,Price:req.body.Price,Duration:req.body.Duration}).then(()=>{
-                res.status(214).json(updatedBook)
-            }).catch((err)=>{
-                res.status(421).send(err)
-            })
-        }
-    })
+    const exists=await BookModal.find({_id:req.params.id})
+    if(exists.length){
+        const updatedbook=await BookModal.updateOne({_id:req.params.id},{$set:{Name:req.body.Name,Author:req.body.Author,Price:req.body.Price,Duration:req.body.Duration}});
+        res.status(212).json(req.body)
+    }else{
+        res.status(402).send('create new one ')
+    }
+   
 })
+
 module.exports=router
 
 // //await BookModal.findByIdAndUpdate(id,{Name:req.body.Name,Author:req.body.Author,Price:req.body.Price,Duration:req.body.Duration},(err,data)=>{
